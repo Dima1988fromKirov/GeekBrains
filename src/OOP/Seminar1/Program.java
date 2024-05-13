777825fcca1c6e878cb2c17ebc1e2128a1420c0e
Dmitry <88_d1m@mail.ru>
2024-05-13 17:58:13 +0300

    test 1

--- src/OOP/Seminar1/Program.java
+++ src/OOP/Seminar1/Program.java
@@ -2,7 +2,8 @@ package OOP.Seminar1;
 
 public class Program {
     public static void main(String[] args) {
-        Product product1 = new BottleOfWater("Меркурий",100,1);
+        Product product1 = new BottleOfWater("BonAqua1",100,1);
+        Product product2 = new BottleOfWater("BonAqua2",100,1);
         System.out.println(product1.toString());
     }
 }
