93710fa233e338e86f19548602c829a68c6af066
Dmitry <88_d1m@mail.ru>
2024-05-13 17:42:51 +0300

    Initial commit дз семинар1

---
+++ src/OOP/Seminar1/Program.java
@@ -0,0 +1,8 @@
+package OOP.Seminar1;
+
+public class Program {
+    public static void main(String[] args) {
+        Product product1 = new BottleOfWater("Меркурий",100,1);
+        System.out.println(product1.toString());
+    }
+}
